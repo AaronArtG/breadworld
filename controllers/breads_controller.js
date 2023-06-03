ba3d1fe9240fcfd815f8c6825d2d71788de92ffa
Aaron garcia <aarongarcia@Aarons-MacBook-Pro.local>
2023-06-03 13:31:33 -0700

    added a image and input buttons

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -10,6 +10,12 @@ breads.get('/', (req, res) => {
 //   res.send(Bread)
 })
 
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
+})
+
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -20,7 +26,22 @@ breads.get('/:arrayIndex', (req, res) => {
       res.send('404')
     }
   })
-  
+
+// CREATE
+breads.post('/', express.urlencoded({ extended: true }),(req, res) => {
+  // console.log(req.body)
+  if(!req.body.image){
+    req.body.image = 
+    'https://plus.unsplash.com/premium_photo-1674939148553-9ca55be9b037?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60'
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = 'true'
+  } else {
+    req.body.hasGluten = 'false'
+  }
+  Bread.push(req.body)
+  res.redirect("/breads")
+})
 
 
 
