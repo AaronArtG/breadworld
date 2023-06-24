18e2d0477064f4397ff866d8c6a8e5dc5723be53
Aaron garcia <aarongarcia@Aarons-MBP.attlocal.net>
2023-06-24 13:48:51 -0700

    new

--- views/show.jsx
+++ views/show.jsx
@@ -1,28 +1,27 @@
 const React = require("react");
 const Default = require("./layout/default");
 
-function Show({ bread, index }) {
+function Show({ bread }) {
   console.log(bread.name);
   return (
     <Default>
       <h3>{bread.name}</h3>
-      <p>
-        and it
-        {bread.hasGluten ? <span> does </span> : <span> does NOT </span>}
-        have gluten.
-      </p>
-      <img src={bread.image} alt={bread.name} />
-      <p>{bread.getBakedBy()}</p>
-      <li>
-        <a href="/breads">Go home</a>
-      </li>
-      <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
-        <input type="submit" value="DELETE" />
-      </form>
-      <br />{" "}
-      <a href={`/breads/${bread.id}/edit`}>
-        <button>Edit</button>
-      </a>
+  <p>
+    and it
+    {
+      bread.hasGluten
+      ? <span> does </span>
+      : <span> does NOT </span>
+    }
+    have gluten.
+  </p>
+  <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+  <img src={bread.image} alt={bread.name} />
+  <p>Baked by {bread.getBakedBy()}</p>
+  <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
+    <input type="submit" value="DELETE"/>
+  </form>
+  <li><a href="/breads">Go home</a></li>
     </Default>
   );
 }
